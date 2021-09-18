a2b3c752ba374f9f94ae438dadcd4b61f928720c
Eric Miner <ericminer78@gmail.com>
2021-09-17 23:16:18 -0500

    added styles.css to CreateList pages

--- src/components/CreateWList.js
+++ src/components/CreateWList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/styles.css";
 
 export default class CreateWList extends Component {
   constructor(props) {
